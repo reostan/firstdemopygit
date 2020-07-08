eb29f2fb58a06f034234ea80e92edb5e95bb9a85
reostan <reostan32325@gmail.com>
2020-07-08 12:41:06 +0545

    binary search in a list

--- best.py
+++ best.py
@@ -1,2 +1,28 @@
-print("Hello world")
-print("new line")
+p = 0
+
+
+def search(lst, n):
+    l = 0
+    u = len(lst) - 1
+
+    while l <= u:
+        mid = (l + u)//2
+        if lst[mid] == n:
+            globals()['p'] = mid
+
+            return True
+
+        else:
+            if lst[mid] < n:
+                l = mid + 1
+            else:
+                u = mid - 1
+    return False
+
+
+lst = [4, 5, 6, 7, 8, 9]
+n = 5
+if search(lst, n):
+    print(f"Found at {p + 1}")
+else:
+    print("not found")
